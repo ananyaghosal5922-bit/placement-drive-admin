9a7438f55ae3e8db88334e2baabfd3ec43975912
ananyaghosal5922-bit <ananyaghosal5922@gmail.com>
2026-07-03 20:14:51 +0530

    added company search feature

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import useDriveStore from "../store/DriveStore";
 
 function Dashboard() {
   const { drives, deleteDrive } = useDriveStore();
+  const [search, setSearch] = useState("");
+
+  const filteredDrives = drives.filter((drive) =>
+  drive.companyName.toLowerCase().includes(search.toLowerCase())
+);
  
   console.log("Drives:",drives);
   return (
@@ -12,10 +18,22 @@ function Dashboard() {
       <Link to="/create">
         <button>Create New Drive</button>
       </Link>
+      <br />
+<br />
+
+<input
+  type="text"
+  placeholder="Search by Company Name"
+  value={search}
+  onChange={(e) => setSearch(e.target.value)}
+/>
+
+<br />
+<br />
     {drives.length === 0 ? (
   <p>No Placement Drives Available</p>
   ) : (
-  drives.map((drive, index) => (
+  filteredDrives.map((drive, index) => (
     <div key={index}>
       <h3>{drive.companyName}</h3>
       <p>{drive.role}</p>
